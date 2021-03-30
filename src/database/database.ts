f260733e3be1f2c5072073f912ac30a32c9eae8f
João Pedro <pedrobarros2010@gmail.com>
2021-03-30 19:18:35 -0300

    fix: add timezone

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,13 +3,17 @@ import { resolve } from 'path'
 
 require('dotenv/config')
 
-export default new Sequelize(
+const connection = new Sequelize(
   process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
     host: process.env.DATABASE_HOST,
     dialect: 'postgres',
+    // eslint-disable-next-line
     models: [resolve(__dirname, '..', 'models')],
+    timezone: '-03:00',
   }
 )
+
+export default connection
