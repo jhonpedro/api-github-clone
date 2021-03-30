4fc4b7bdb1f8c2afd03f4ae323daa4fd22e76288
João Pedro <pedrobarros2010@gmail.com>
2021-03-30 16:39:45 -0300

    feat: add AppError and error middleware

---
+++ src/utils/errors/ErrorHandlerMiddleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express'
+import AppError from './AppError'
+
+export default function ErrorHandlerMiddleware(
+  err,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line
+  next: NextFunction
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message })
+  }
+
+  return res.status(500).json({ messsage: err.message })
+}
