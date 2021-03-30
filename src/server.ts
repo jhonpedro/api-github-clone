6a3e3514225a3c7d9741705c9d9c903b531faf3e
João Pedro <pedrobarros2010@gmail.com>
2021-03-30 19:17:47 -0300

    fix: synchronizing models

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import App from './App'
 
-App.listen('8080', () => {
+import Connection from './database/database'
+
+App.listen('8080', async () => {
   // eslint-disable-next-line
   console.log('Server is running on 8080')
+
+  try {
+    await Connection.authenticate()
+    Connection.sync({ force: true })
+  } catch (err) {
+    // eslint-disable-next-line
+    console.log(err)
+  }
 })
