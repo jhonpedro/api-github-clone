9585d007502a6405b94c5d09daf632af87fea99f
João Pedro <pedrobarros2010@gmail.com>
2021-03-31 15:39:16 -0300

    feat: add new update route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
+import multer from 'multer'
 import UserController from '../controllers/UserController'
+import multerConfig from '../config/multer'
+import auth from '../middlewares/auth'
+
+const upload = multer(multerConfig)
 
 const UserRoutes = Router()
 
 UserRoutes.get('/:userName', UserController.show)
 UserRoutes.post('/', UserController.create)
 UserRoutes.post('/session', UserController.session)
+UserRoutes.put('/', auth, upload.single('avatar'), UserController.update)
 
 export default UserRoutes
