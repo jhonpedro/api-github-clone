9199a6128381446759cbbd4c236bf9482d532a30
João Pedro <pedrobarros2010@gmail.com>
2021-04-01 00:10:52 -0300

    refactor: remove exception treatment block

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,6 @@ App.listen('8080', async () => {
   // eslint-disable-next-line
   console.log('Server is running on 8080')
 
-  try {
-    await Connection.authenticate()
-    Connection.sync({ force: true })
-  } catch (err) {
-    // eslint-disable-next-line
-    console.log(err)
-  }
+  await Connection.authenticate()
+  Connection.sync({ force: true })
 })
