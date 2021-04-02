1240e85b4b26dfc5b65d1d062189dcb46124bece
João Pedro <pedrobarros2010@gmail.com>
2021-04-02 14:18:29 -0300

    feat: add follows and following routes

--- src/routes/FollowerRoutes.ts
+++ src/routes/FollowerRoutes.ts
@@ -4,6 +4,8 @@ import auth from '../middlewares/auth'
 
 const FollowerRoutes = Router()
 
+FollowerRoutes.get('/:userName/follows', FollowerController.indexFollowers)
+FollowerRoutes.get('/:userName/following', FollowerController.indexFollowing)
 FollowerRoutes.post('/follow/:username', auth, FollowerController.follow)
 FollowerRoutes.post('/unfollow/:username', auth, FollowerController.unfollow)
 
