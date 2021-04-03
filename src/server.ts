94a6b329ab460199a17d0f0209b38bcc1f839a18
João Pedro <pedrobarros2010@gmail.com>
2021-04-03 14:23:29 -0300

    refactor: try to connect multiple times to database before error

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,26 @@ import Connection from './database/database'
 
 App.listen('8080', async () => {
   // eslint-disable-next-line
-  console.log('Server is running on 8080')
 
-  await Connection.authenticate()
-  Connection.sync({ force: true })
+  // Try to connect to database five times
+  let succesToConnect = false
+  for (let i = 0; i < 5; i += 1) {
+    try {
+      console.log(`Try ${i + 1} to connect`)
+      await Connection.authenticate()
+      Connection.sync({ force: false })
+      console.log('connected to db')
+      succesToConnect = true
+      break
+    } catch {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 3000)
+      })
+      continue
+    }
+  }
+  if (!succesToConnect) {
+    throw Error('Error to connect to database')
+  }
+  console.log('Server is running on 8080')
 })
