1506cee367f175a5040a91cc1a73d6ba4ad9605f
João Pedro <pedrobarros2010@gmail.com>
2021-04-23 18:57:59 -0300

    wip: add sing in page to routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Main from '../pages/Main'
+import SingIn from '../pages/SingIn'
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/">
-        <Main />
+      <Route path="/sing-in">
+        <SingIn />
       </Route>
     </Switch>
   )
