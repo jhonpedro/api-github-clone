3e2021f9ae77971775387f40492f0aecd51c45e2
João Pedro <pedrobarros2010@gmail.com>
2021-04-25 22:57:32 -0300

    fix: spread props in button

--- web/src/components/Button/index.tsx
+++ web/src/components/Button/index.tsx
@@ -5,12 +5,12 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ children, size }) => {
+const Button: React.FC<ButtonProps> = ({ children, size, ...rest }) => {
   const sizeToContainer = !size ? 'auto' : size
 
   return (
     <ButtonContainer size={sizeToContainer}>
-      <ButtonElement>{children}</ButtonElement>
+      <ButtonElement {...rest}>{children}</ButtonElement>
     </ButtonContainer>
   )
 }
