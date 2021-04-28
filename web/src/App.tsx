1106f5ab21bb146a735a792e76e757498037bbfb
João Pedro <pedrobarros2010@gmail.com>
2021-04-27 21:15:26 -0300

    feat: add provider and store

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import Routes from './routes'
 import GlobalStyle from './assets/global'
+import store from './store/store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
