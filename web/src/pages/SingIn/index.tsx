3fb33cbf4ffd7854ce0b0636ef417d6712f43ad7
João Pedro <pedrobarros2010@gmail.com>
2021-04-27 21:21:06 -0300

    feat: calling dispatch from react redux to login

--- web/src/pages/SingIn/index.tsx
+++ web/src/pages/SingIn/index.tsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
 import { DiGithubBadge } from 'react-icons/di'
 import { FaArrowRight } from 'react-icons/fa'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+import { SingIn } from '../../store/actions'
 import { SingInContainer } from './styles'
 
 const LoginPage = () => {
   const [username, setUsername] = useState('')
+  const dispatch = useDispatch()
+  const { push } = useHistory()
 
-  async function handleChangeUsername(
-    event: React.ChangeEvent<HTMLInputElement>
-  ) {
+  async function handleSingIn() {
+    dispatch(
+      SingIn(
+        username,
+        ({
+          token,
+          username: usernameInState,
+        }: {
+          token: string
+          username: string
+        }) => {
+          localStorage.setItem('token', token)
+          localStorage.setItem('username', usernameInState)
+
+          push('/')
+        }
+      )
+    )
+  }
+
+  function handleChangeUsername(event: React.ChangeEvent<HTMLInputElement>) {
     setUsername(event.target.value)
   }
 
@@ -24,7 +47,7 @@ const LoginPage = () => {
         placeholder="Username"
         onChange={handleChangeUsername}
       />
-      <Button size="80%">
+      <Button size="80%" onClick={handleSingIn}>
         ENTRAR <FaArrowRight color="var(--color-dark)" />{' '}
       </Button>
     </SingInContainer>
