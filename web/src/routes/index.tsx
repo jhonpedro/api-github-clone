748c97781900947f9988e094c0562bbb87906a3d
João Pedro <pedrobarros2010@gmail.com>
2021-04-28 22:34:34 -0300

    feat: add userprofile route

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import UserProfile from '../components/UserProfile'
 import SingIn from '../pages/SingIn'
 
 function Routes() {
   return (
     <Switch>
+      <Route path="/">
+        <UserProfile />
+      </Route>
       <Route path="/sing-in">
         <SingIn />
       </Route>
