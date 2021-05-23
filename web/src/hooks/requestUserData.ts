271437f0353df9c96d4a25399bd1439799262794
João Pedro <pedrobarros2010@gmail.com>
2021-05-23 14:43:35 -0300

    feart: create request user data hook

---
+++ web/src/hooks/requestUserData.ts
@@ -0,0 +1,39 @@
+import { AxiosResponse } from 'axios'
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from '../services/axios'
+
+export interface UserI {
+  id: number
+  name: string
+  email: string
+  localization: string
+  avatar: string
+  username: string
+  bio: string
+  createdAt: Date
+  updatedAt: Date
+  followersCount: number
+  followingCount: number
+  repositoriesCount: number
+}
+
+const useRequestUserData = (username: string | undefined) => {
+  const [user, setUser] = useState({} as UserI)
+  const { push } = useHistory()
+
+  useEffect(() => {
+    if (username) {
+      axios
+        .get<any, AxiosResponse<UserI>>(`users/${username}`)
+        .then((response) => {
+          setUser(response.data)
+        })
+        .catch(() => push('/sing-in'))
+    }
+  }, [username])
+
+  return user
+}
+
+export default useRequestUserData
