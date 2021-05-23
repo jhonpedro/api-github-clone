a372044b70f56ac905ae7db46aaa52ba1ac91e72
João Pedro <pedrobarros2010@gmail.com>
2021-05-23 14:57:22 -0300

    refactor: remove interface and use request user data hook

--- web/src/pages/User/index.tsx
+++ web/src/pages/User/index.tsx
@@ -1,28 +1,12 @@
-import { AxiosResponse } from 'axios'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import { FiEdit, FiLogOut } from 'react-icons/fi'
 import { useSelector } from 'react-redux'
-import { useHistory, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import UserProfile from '../../components/UserProfile'
-import axios from '../../services/axios'
+import useRequestUserData from '../../hooks/requestUserData'
 import userSelector from '../../store/selectors/user'
 
 import { ActionButtons, UserContainer } from './styles'
-
-export interface UserI {
-  id: number
-  name: string
-  email: string
-  localization: string
-  avatar: string
-  username: string
-  bio: string
-  createdAt: Date
-  updatedAt: Date
-  followersCount: number
-  followingCount: number
-  repositoriesCount: number
-}
 
 interface UserRouteParams {
   username: string | undefined
@@ -32,20 +16,7 @@ function User() {
   const { username: usernameParam } = useParams<UserRouteParams>()
   const username = useMemo(() => usernameParam, [usernameParam])
   const { username: loggedUserUsername } = useSelector(userSelector)
-  const { push } = useHistory()
-
-  const [user, setUser] = useState({} as UserI)
-
-  useEffect(() => {
-    axios
-      .get<any, AxiosResponse<UserI>>(`users/${username}`)
-      .then((response) => {
-        setUser(response.data)
-      })
-      .catch(() => {
-        push('/sing-in')
-      })
-  }, [username])
+  const user = useRequestUserData(username)
 
   function editMyself() {}
 
