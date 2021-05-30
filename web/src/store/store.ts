afd0a951c2ff445dd00d223209cd08342798e367
João Pedro <pedrobarros2010@gmail.com>
2021-05-30 12:10:11 -0300

    feat: use devtools extension

--- web/src/store/store.ts
+++ web/src/store/store.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import createSagaMiddleware from 'redux-saga'
 import reducers from './reducers'
 import authSaga from './reducers/auth/sagas'
@@ -6,7 +7,10 @@ import authSaga from './reducers/auth/sagas'
 // Create saga middleware
 const sagaMiddleware = createSagaMiddleware()
 // Aply the middleware
-const store = createStore(reducers, applyMiddleware(sagaMiddleware))
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
 // Run saga middleware
 sagaMiddleware.run(authSaga)
 
